a58d5143010577fa9ef3a860e8b07dd72e30485d
Eugenia-Uvarov <grungefortuna@gmail.com>
2023-01-03 16:29:58 -0500

    Make grocery list

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import image from './basket.png';
+import { GroceryList } from './GroceryList';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+	<div className="App">
+    <div className="container">
+      <h1>Grocery List</h1>
     </div>
+    <GroceryList/>
+    <div className="container">
+      <img src={ image } width="200" alt="Shopping cart" className="image"/>
+    </div>
+	</div>
+  
+  <footer>
+      <p>All materials were used for educational purposes.</p> 
+  </footer>
+  </div>
   );
 }
 
